44f9950b3efb74e842eaffa462c3a6c9271d5112
Vicente Dario Alvarez Herrera <vidaalhe90@gmail.com>
2025-05-30 03:05:13 -0600

    Exercise 2.16 solved (C How to Program)

--- ebook_c_how_to_program_deitel/chapter02/exercise02_16.c
+++ ebook_c_how_to_program_deitel/chapter02/exercise02_16.c
@@ -10,22 +10,28 @@
 /* function main begins program execution */
 int main(void) {
 
+    /* enter two integers */
     int number1, number2;
     printf("%s", "Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
+    /* compute and show their sum */
     int sum = number1 + number2;
     printf("%d + %d = %d\n", number1, number2, sum);
 
+    /* compute and show their product */
     int product = number1 * number2;
     printf("%d x %d = %d\n", number1, number2, product);
 
+    /* compute and show their difference */
     int difference = number1 - number2;
     printf("%d - %d = %d\n", number1, number2, difference);
 
+    /* compute and show their quotient */
     int quotient = number1 / number2;
     printf("%d / %d = %d\n", number1, number2, quotient);
 
+    /* compute and show their remainder */
     int remainder = number1 % number2;
     printf("%d %% %d = %d\n", number1, number2, remainder);
 
