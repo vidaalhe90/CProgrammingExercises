dba049153ed69f2c0573db065ffe8b38e986af08
Vicente Dario Alvarez Herrera <vidaalhe90@gmail.com>
2025-05-30 02:39:05 -0600

    Excersise 2.7 solved (C How to Program)

---
+++ ebook_c_how_to_program_deitel/chapter02/exercise02_27.c
@@ -0,0 +1,38 @@
+
+/* exercise02_27.c - Summing the Digits of an Integer
+   
+   Write a program that inputs one 4-digit number, sums each
+   of the individual digits, and then displays the result.
+   [Hint: Use division and remainder operation] */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    /* enter a 4-digit number*/
+    int number;
+    printf("%s", "Enter a 4-digit number: ");
+    scanf("%d", &number);
+    
+    /* find units digit and update number. */
+    int units = number % 10;
+    number = number / 10;
+
+    /* find tens digit and update number. */
+    int tens = number % 10;
+    number = number / 10;
+
+    /* find hundreds digit and update number. */
+    int hundreds = number % 10;
+    number = number / 10;
+
+    /* find thousands digit (no necessary update number)*/
+    int thousands = number % 10;
+
+    /* sums all digits and display the result. */
+    int total = units + tens + hundreds + thousands;
+    printf("%d + %d + %d + %d = %d\n", thousands, hundreds, tens, units, total);
+
+    return EXIT_SUCCESS;
+}
