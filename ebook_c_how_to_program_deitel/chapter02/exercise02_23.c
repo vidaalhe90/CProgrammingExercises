6c2ecebd07684be4ea643bbe028bc707fbb5f4ae
Vicente Dario Alvarez Herrera <vidaalhe90@gmail.com>
2025-05-30 23:28:41 -0600

    Exercise 2.22 solved (C How to Program)

---
+++ ebook_c_how_to_program_deitel/chapter02/exercise02_23.c
@@ -0,0 +1,27 @@
+
+/* exercise02_23.c - Multiples
+
+   Write a program that reads two integers and determines and displays whether
+   the first is a multiple of the second. Use the remainder operator. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    /* enter numbers */
+    int a, b;
+    printf("%s", "Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    /* if b is divisible by a, then a is is a multiple of b */
+    if (b % a == 0) {
+        printf("%d is a multiple of %d\n", a, b);
+        return EXIT_SUCCESS;
+    }
+
+    /* at this point, a is not a multiple of b */
+    printf("%d is not a multiple of %d\n", a, b);
+
+    return EXIT_SUCCESS;
+}
