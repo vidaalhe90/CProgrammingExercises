d09c561ee64b0606ffc98a5c6971be9509b5ad9b
Vicente Dario Alvarez Herrera <vidaalhe90@gmail.com>
2025-05-31 01:27:04 -0600

    Exercise 7.5 solved (Introduction to Programming)

---
+++ ebook_intro_to_programming_liang/chapter07/exercise07_05.c
@@ -0,0 +1,32 @@
+
+/* exercise07_05.c - The number of even numbers and odd numbers.
+
+   Write a program that reads ten integers, and the  displays the
+   number of even numbers and odd numbers. Assume that the input ends
+   with 0. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* function main begins program execution */
+int main(void) {
+
+    int ocurrences[] = {0, 0};
+    int input;
+
+    printf("%s", "Enter numbers: ");
+    for (;;) {
+        scanf("%d", &input);
+        if (input == 0) {
+            break;
+        }
+        ocurrences[input % 2]++;
+    }
+
+    printf("The number of even numbers: %d\n", ocurrences[0]);
+    printf("The number of odd numbers: %d\n", ocurrences[1]);
+    
+    return EXIT_SUCCESS;
+}
