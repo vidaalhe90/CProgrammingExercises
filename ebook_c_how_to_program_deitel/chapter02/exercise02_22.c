d5d5d01dc3090d6ed22e7ee16404009006d01544
Vicente Dario Alvarez Herrera <vidaalhe90@gmail.com>
2025-05-30 23:11:49 -0600

    Exercise 2.22 solved (C How to Program)

---
+++ ebook_c_how_to_program_deitel/chapter02/exercise02_22.c
@@ -0,0 +1,28 @@
+
+/* exercise02_22.c - Odd of Even
+   
+   Write a program that reads an integer and determines and displays whether it's odd or even.
+   Use the remainder operator. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function main begins program execution */
+int main(void) {
+
+    /* enter number */
+    int n;
+    printf("%s", "Enter an integer: ");
+    scanf("%d", &n);
+
+    /* if n is even prints appropriate message and ends program execution */
+    if (n % 2 == 0) {
+        printf("%d is even\n", n);
+        return EXIT_SUCCESS;
+    }
+
+    /* at this point n is odd */
+    printf("%d is odd\n", n);
+
+    return EXIT_SUCCESS;
+}
